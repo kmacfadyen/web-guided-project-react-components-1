b54168e37767513660debf56a5d10f7fbcdebf8c
Kevin MacFadyen <kmacfadyen3@gmail.com>
2022-12-19 02:33:35 -0500

    completed

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -8,3 +8,54 @@
   - We never tamper with state: `healthPoints++`, `healthPoints--` or `someState.push(item)` is FORBIDDEN
   - We use the dedicated "state updater" to schedule a state change: `setHealthPoints(healthPoints + 1)`
 */
+import React, { useState } from 'react';
+
+function Playground(props) {
+  const [count, setCount] = useState(0);
+  const [spinnerOn, setSpinnerOn] = useState(false);
+  const [weapon, setWeapon] = useState("scissors");
+  // const state = useState(0);
+  // const count = state[0];
+  // const setCount = state[1];
+  console.log(count);
+// count = count + 500;
+setCount(count + 500);
+// const newCount = count;
+// newCount = newCount + 500;
+// return newCount;
+
+if(spinnerOn){
+  return (
+    <div className='container'>
+      <h3>The spinner is {spinnerOn ? "ON" : "OFF"}</h3>
+      <button onClick={() => setSpinnerOn(false)}>Turn spinner off</button>
+    </div>
+  )
+}
+
+// updateName = () => {
+//   setName("Kevin");
+// }
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button> 
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <h3>The spinner is {spinnerOn ? "ON" : "OFF" }</h3>
+      <button onClick={() => setSpinnerOn(!spinnerOn)}>Toggle Spinner</button>
+      <h3>The current weapon is: {weapon}</h3>
+      <button onClick={ ()=> setWeapon("scissors")}>Pick Scissors</button>
+      <button onClick={ ()=> setWeapon("rock")}>Pick Rock</button>
+      <button onClick={ ()=> setWeapon("paper")}>Pick Paper</button>
+
+
+      {/* <button onClick={() => setCount(count + 1)}>Uh oh</button> */}
+
+      {/* <button onClick={updateName}>set your name!</button> */}
+    </div>
+  )
+}
+// This creates an increment button which you can "count" up with, and a decrement button to count down
+
+export default Playground;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Playground from './components/Playground';
 
 /*
 💥💥💥 Rules when DECLARING a React component 💥💥💥
@@ -21,15 +22,36 @@ import { render } from 'react-dom'
   - Don't forget all tags need to close correctly
 */
 
+// React has several files in several components
+
 function App(props) {
   return (
+    // This is JSX
     <div className='container'>
+      {/** const elem = document.createElement("div") 
+       * elem.classList.add("container");
+       * document.body.appendChild(elem);
+       * 
+       * Invoke Playground (Capital letter shows JS that it is an import)
+      */}
       <h1>Welcome to React, {props.track} rockstars!!</h1>
+      <h2>Instructor name: {props.name}, age: {props.age}</h2>
+      <h3>Are you happy? {props.happy}</h3>
+      <Playground /> 
     </div>
   )
 }
 
+// props: Used to add an unlimited number or arguments
+
 render(
-  <App track='Web' />,
+  <App track='Web' name="Kevin" happy="yes" />,
   document.querySelector('#root')
 )
+
+const props = {
+  track: "Web",
+  name: "Kevin",
+  happy: "Yes"
+}
+App()
